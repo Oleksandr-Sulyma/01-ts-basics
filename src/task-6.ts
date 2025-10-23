dfa39750616465180a51dc39d1f64556dd9da6d5
Oleksandr Sulyma <sulima.ab@gmail.com>
2025-10-23 10:11:03 +0300

    Initial commit

---
+++ src/task-6.ts
@@ -0,0 +1,14 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+getFirstElement<number>([1, 2, 3]); // 1
+getFirstElement<string>(["a", "b", "c"]); // "a"
+getFirstElement<boolean>([true, false, true]); // true
+getFirstElement<boolean>([5, "false", true]); // Type 'number' is not assignable to type 'boolean'.ts(2322)
+
+console.log(getFirstElement<number>([1, 2, 3]));
+console.log(getFirstElement<string>(["a", "b", "c"]));
+console.log(getFirstElement<boolean>([true, false, true]));
+console.log(getFirstElement<boolean>([5, "false", true]));
+
